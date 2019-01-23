21e4540b94eb84f8210cc795437d8c82a2351e39
Monika Fac <monika.fac@gmail.com>
2019-01-23 21:23:58 +0100

    contact, menu ready

--- main.js
+++ main.js
@@ -13,19 +13,34 @@ const OfferHeight = document.querySelector('.offer').clientHeight;
 const offerFromBeggining = document.querySelector('.offer').offsetTop;
 
 if (scrollSize < BackgroundHeight) {
-   home.style.color="red";
+   home.style.color="#1DE9B6";
    offer.style.color="white";
    contact.style.color="white";
    } else if (scrollSize < OfferHeight + offerFromBeggining) {
-    offer.style.color="blue";
+    offer.style.color="#1DE9B6";
     contact.style.color="white";
     home.style.color="white";
    } else{
-    contact.style.color="black";
+    contact.style.color="#1DE9B6";
     offer.style.color="white";
     home.style.color="white";
    }
 }
 
 
-window.addEventListener('scroll', changeMenu)
+
+//przechodzenie do sekcji strony po kliknięciu
+
+function changeSection(button, section){
+    $(button).on('click', function(){
+        $("body, html").animate({
+            scrollTop: $(section).offset().top
+        },500)
+    })
+}
+
+
+window.addEventListener('scroll', changeMenu);
+changeSection('.menuHome', ".home");
+changeSection('.menuOffer', '.offer');
+changeSection('.menuContact', '.contact');
